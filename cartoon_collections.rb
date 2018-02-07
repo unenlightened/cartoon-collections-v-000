abbeed8741a24bc7c705af9ca12822a0a25d6d61
Chely Ho <adept_inept@yahoo.com>
2018-02-07 20:52:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,12 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |each|
+    cheese_type.any? do |cheese|
+      each == cheese
+    end
+  end
+
 end
